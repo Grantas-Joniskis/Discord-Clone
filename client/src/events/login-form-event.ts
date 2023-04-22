8a111bb59d4312167faf60208dfeb63d3d9991a7
Grantas-Joniskis <grantasjo@gmail.com>
2023-04-22 14:53:22 +0300

    Creating user list.

--- client/src/events/login-form-event.ts
+++ client/src/events/login-form-event.ts
@@ -3,6 +3,7 @@ import { FormEvent } from 'react';
 import AxiosService from 'services/axios/axios-service';
 import SocketioService from 'services/socketio/socketio-service';
 import LoginUser from 'types/login-user';
+import CurrentUser from 'user/CurrentUser';
 import validateLoginInput from 'validation/login-form-validation';
 
 const handleLogin = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
@@ -19,10 +20,13 @@ const handleLogin = async (event: FormEvent<HTMLFormElement>): Promise<void> => 
 
   try {
     const response = await AxiosService.postLoginUser(loginUser);
+    const { user } = response.data;
     SocketioService.createConnection(response.data.bearer);
+    console.log(response.data.bearer);
+    CurrentUser.create(response.data.bearer, user.id, user.username);
     console.log('Response:', response);
 
-    // DIRTY AF
+    // DIRTY AF (window.location.assign())
     window.location.assign(routes.Chat);
   } catch (error) {
     console.error('Error:', error);
