16e92202188ea540406402e58f40ac60e8d64f70
Grantas-Joniskis <grantasjo@gmail.com>
2023-04-25 10:15:55 +0300

    Renaming style folder.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'style/colors.css';
-import 'style/main.css';
+import 'styles/colors.css';
+import 'styles/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { RouterProvider } from 'react-router-dom';
